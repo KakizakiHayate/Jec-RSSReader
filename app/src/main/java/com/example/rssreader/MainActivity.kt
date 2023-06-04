3f993c4534d83fe0acd2ddc98683cae5c8f88869
kakizaki hayate <hayate.h.0704@gmail.com>
2023-06-04 17:32:08 +0900

    ListViewの項目をクリックしたらToastにタップしたURLが表示される

--- app/src/main/java/com/example/rssreader/MainActivity.kt
+++ app/src/main/java/com/example/rssreader/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.rssreader
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
 import android.widget.TextView
+import android.widget.Toast
 import org.w3c.dom.Text
 import java.io.BufferedReader
 import java.io.IOError
@@ -16,20 +24,34 @@ import java.lang.Exception
 import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var adapter: RowModelAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnLoad = findViewById<Button>(R.id.btnLoad)
-        btnLoad.setOnClickListener {
-            var sb = StringBuilder()
-            val arrayList = JsonHelper.parseJson(getData())
-            for (tmp in arrayList ) {
-                sb.append(tmp.title + "\n")
-                sb.append(tmp.link + "\n")
-            }
-            val txtResult = findViewById<TextView>(R.id.txtResult)
-            txtResult.text = sb.toString()
+//        val btnLoad = findViewById<Button>(R.id.btnLoad)
+//        btnLoad.setOnClickListener {
+//            var sb = StringBuilder()
+//            val arrayList = JsonHelper.parseJson(getData())
+//            for (tmp in arrayList ) {
+//                sb.append(tmp.title + "\n")
+//                sb.append(tmp.link + "\n")
+//            }
+//            val txtResult = findViewById<TextView>(R.id.txtResult)
+//            txtResult.text = sb.toString()
+//        }
+        adapter = RowModelAdapter(this)
+        val arrayList = JsonHelper.parseJson(getData())
+        for (item in arrayList) {
+            adapter.add(item)
+        }
+
+        var list = findViewById<ListView>(R.id.resultList)
+        list.adapter = adapter
+        list.setOnItemClickListener { adapterView, view, position, id ->
+            // ここにリストの項目がクリックされたときの処理を書く
+            val item = adapterView.adapter.getItem(position) as RssItem
+            Toast.makeText(this@MainActivity, item.link, Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -37,5 +59,27 @@ class MainActivity : AppCompatActivity() {
         val inputStream = resources.assets.open("rss.json")
         val jsonString = inputStream.bufferedReader().use { it.readText() }
         return jsonString
+    }
+
+
+        inner class RowModelAdapter(context: Context): ArrayAdapter<RssItem>(context, R.layout.row_item) {
+         override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+             val item = getItem(position) as RssItem
+             var convertView = convertView
+                 var inflater = layoutInflater
+                 convertView = inflater.inflate(R.layout.row_item, null)
+
+             if (item != null) {
+                 val txtTitle = convertView?.findViewById<TextView>(R.id.txtTitle)
+                 if (txtTitle != null) {
+                     txtTitle.text = item.title
+                 }
+                 val txtlink = convertView?.findViewById<TextView>(R.id.txtLink)
+                 if (txtlink != null) {
+                     txtlink.text = item.link
+                 }
+             }
+             return convertView
+         }
     }
 }
